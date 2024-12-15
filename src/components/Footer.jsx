ab30d1bb3211affa860d5136f60c12786481ed21
Kamyar Ganjian <kamyarganjian@gmail.com>
2024-12-15 21:11:44 +0330

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,43 @@
+import { FaDiscord, FaTwitter, FaYoutube, FaMedium } from "react-icons/fa";
+
+const socialLinks = [
+  { href: "https://discord.com", icon: <FaDiscord /> },
+  { href: "https://twitter.com", icon: <FaTwitter /> },
+  { href: "https://youtube.com", icon: <FaYoutube /> },
+  { href: "https://medium.com", icon: <FaMedium /> },
+];
+
+const Footer = () => {
+  return (
+    <footer className="w-screen bg-red-700 py-4 text-white px-2">
+      <div className="container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row">
+        <p className="text-center text-sm font-light md:text-left">
+          ©Kamyar Ganjian 2024. All rights reserved
+        </p>
+
+        <div className="flex justify-center gap-4  md:justify-start">
+          {socialLinks.map((link, index) => (
+            <a
+              key={index}
+              href={link.href}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-white transition-colors duration-500 ease-in-out"
+            >
+              {link.icon}
+            </a>
+          ))}
+        </div>
+
+        <a
+          href="#privacy-policy"
+          className="text-center text-sm font-light hover:underline md:text-right"
+        >
+          Privacy Policy
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
